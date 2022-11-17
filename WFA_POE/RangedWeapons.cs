45233279d88bd79b8f9a468b7df754e9e7a04e83
Malcom Thonger <malcozui@gmail.com>
2022-11-17 07:16:20 +0200

    range class fixed

--- WFA_POE/RangedWeapons.cs
+++ WFA_POE/RangedWeapons.cs
@@ -4,9 +4,31 @@ namespace WFA_POE
 {
     internal class RangedWeapon : Weapon
     {
-        public RangedWeapon(int x, int y) : base(x, y)
+        public enum Type
         {
+            Longbow,
+            Rifle
+        }
 
+        public RangedWeapon(Type weaponType, int x = -1, int y = -1) : base(x, y)
+        {
+            switch (weaponType)
+            {
+                case Type.Longbow:
+                    this.weaponType = "Longbow";
+                    durability = 4;
+                    range = 2;
+                    dmg = 4;
+                    cost = 6;
+                    break;
+                case Type.Rifle:
+                    this.weaponType = "Rifle";
+                    durability = 3;
+                    range = 3;
+                    dmg = 5;
+                    cost = 7;
+                    break;
+            }
         }
 
         public override int Range { get => base.Range; }
